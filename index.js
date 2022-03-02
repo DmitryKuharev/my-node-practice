cfb389e0fb03940a4394ce0f9b634aea6b882572
Dmitry <stee.ou@gmail.com>
2022-03-02 12:50:13 +0300

    Added API

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 import { getArgs } from "./helper/args.js";
-import { printError, printHelp } from "./services/log.service.js";
+import { getWeather } from "./services/api.service.js";
+import { printError, printHelp, printSuccess } from "./services/log.service.js";
+import { saveKeyValue, TOKEN } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    if (!token.length){
+        printError('Token is empty');
+        return;
+    };
+    try {
+        await saveKeyValue(TOKEN.token, token);
+        printSuccess('Token saved');
+    } catch (error) {
+        printError(error.message);
+    }
+    
+}
 
 const start = () => {
     const args = getArgs(process.argv)
@@ -14,7 +30,9 @@ const start = () => {
     }
     if (args.t){
         // Secondary block
+        return saveToken(args.t)
     }
+    getWeather('Mogilev');
 }
 
 start();
